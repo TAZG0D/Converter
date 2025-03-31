d5f3d47332a68b0ef21823766fcd2c60bae00458
TAZG0D <fttx@live.com>
2025-03-31 12:47:14 -0300

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,4 @@
+valorYUAN = prompt("digite um valor em YUAN")
+umYUAN = 0.14
+alert("USD" + valorYUAN * umYUAN)
+alert("O valor em USD é: " + valorUSD.toFixed(2));
---
+++ src/script.js
@@ -0,0 +1,5 @@
+valorYUAN = prompt("digite um valor em YUAN")
+umYUAN = 0.14
+alert("USD" + valorYUAN * umYUAN)
+alert("O valor em USD é: " + valorUSD.toFixed(2));
+
